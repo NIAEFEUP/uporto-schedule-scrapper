ef3d5b4415f236cf51c9c5be6a6e7d6eb6e0e5ff
Bernardo Belchior <bernardo.belchior1@gmail.com>
2017-12-24 18:29:53 +0000

    It is now possible to store the course plan in the database

--- scrapper/scrapper/spiders/plan_spider.py
+++ scrapper/scrapper/spiders/plan_spider.py
@@ -16,13 +16,17 @@ class PlanSpider(scrapy.Spider):
             yield scrapy.Request(url=course[1], meta={'course_id':course[0], 'course_name':course[2]}, callback=self.parse)
 
     def parse(self, response):
+        con_info = ConInfo()
         for planHtml in response.xpath('//*[@id="anos_curr_div"]/div[a]'):
-            course_year = planHtml.xpath("./a/text()").extract_first()
-            print(response.meta['course_name'] + ' - ' + course_year)
 
-            classes = defaultdict(list)
+            # strip whitespaces and get only the first char, which represents the year
+            course_year = planHtml.xpath("./a/text()").extract_first().strip()[:1]
+
             for table in planHtml.xpath('.//table/tr/td/table/tr/td/table'):
-                semester = table.xpath('./tr/th/text()').extract_first()
+
+                # semester may be '1', '2' or 'A', where A means the class is annual
+                semester = table.xpath('./tr/th/text()').extract_first()[:1]
+
                 for class_line in table.xpath('./tr[td/a and string-length(./td[1]/text()) > 0]'):
                     class_info_node = class_line.xpath('./td')
                     class_info = {
@@ -31,6 +35,10 @@ class PlanSpider(scrapy.Spider):
                         'name': class_info_node[2].xpath('./a/text()').extract_first(),
                         'class_url': response.urljoin(class_info_node[2].xpath('./a/@href').extract_first())
                     }
-                    print(class_info)
-                    classes[course_year + '-' + semester].append(class_info)
-            
+
+                    with con_info.connection.cursor() as cursor:
+                        sql = "INSERT INTO class(`year`, `acronym`, `url`, `course_id`) VALUES(%s, %s, %s, %s)"
+                        cursor.execute(sql, (course_year, class_info['acronym'], class_info['class_url'], response.meta['course_id']))
+
+        con_info.connection.commit() # commit the changes
+
