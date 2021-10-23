74e190cec3d7e8e79f45cb1d397db7dab40ec9d4
miguelpduarte <miguelpduarte98@gmail.com>
2021-10-23 16:26:47 +0100

    Update course spider
    Now fetches faculties from previous results.

--- scrape_to_csv/scrape_to_csv/spiders/courses_spider.py
+++ scrape_to_csv/scrape_to_csv/spiders/courses_spider.py
@@ -1,26 +1,33 @@
 import scrapy
+import csv
 from operator import itemgetter
 from urllib.parse import urlparse, parse_qs
+
+FACULTIES_CSV_FILE_PATH = "output2/faculties.csv"
 
 class CoursesSpider(scrapy.Spider):
     name = "courses"
     allowed_domains = ["sigarra.up.pt"]
 
-    raw_courses_url = "https://sigarra.up.pt/feup/pt/cur_geral.cur_tipo_curso_view?pv_tipo_sigla={course_type}&pv_ano_lectivo={school_year}"
+    raw_courses_url = "https://sigarra.up.pt/{faculty}/pt/cur_geral.cur_tipo_curso_view?pv_tipo_sigla={course_type}&pv_ano_lectivo={school_year}"
+
 
     def start_requests(self):
         # TODO: Get from arguments, env variables, etc, somewhere
         year = 2021
 
-        # TODO: Get from the previous results
-        faculties = ['feup', 'fcup']
-        
+        faculties = []
+
+        with open(FACULTIES_CSV_FILE_PATH, "r") as f:
+            csv_data = csv.DictReader(f)
+            faculties = [faculty["acronym"] for faculty in csv_data]
+
         COURSE_TYPES = ['L', 'M', 'MI', 'D']
 
         for faculty in faculties:
             for course_type in COURSE_TYPES:
-                url = self.raw_courses_url.format(course_type=course_type, school_year=year)
-                self.logger.debug(f"Calculated url: {url}")
+                url = self.raw_courses_url.format(faculty=faculty, course_type=course_type, school_year=year)
+                # self.logger.debug(f"Calculated url: {url}")
                 meta_data = {"faculty_acronym": faculty, "course_type": course_type, "year": year}
 
                 yield scrapy.Request(url=url, callback=self.parse_course_list, meta=meta_data)
@@ -50,13 +57,20 @@ class CoursesSpider(scrapy.Spider):
         Direct navigation can't be used since the course_id is not known beforehand.
         """
 
+        # Checks if the course page is pointing elsewhere (see details below)
+        # Previous selector was response.css("body").xpath("//*[@id='conteudoinner']/div[1]/a").get()
+        # This was a true positive for https://sigarra.up.pt/fcup/pt/cur_geral.cur_view?pv_ano_lectivo=2021&pv_origem=CUR&pv_tipo_cur_sigla=D&pv_curso_id=21901
+        # But a false positive for https://sigarra.up.pt/faup/pt/cur_geral.cur_view?pv_ano_lectivo=2021&pv_origem=CUR&pv_tipo_cur_sigla=D&pv_curso_id=45
+        # TODO: Decide if we just ignore the specific page (the one that points to another) since the data would be duplicate; if we scrape the page since the data is hidden but still there, or if we try to follow the link
+        # TODO: Regardless of the above, we have to find a selector that works for one but not the other. Just checking "first div child of div#conteudoinner that has a <a>" will work for both...
         #TODO: Check if the "test if this page points to another one" is necessary...
+
         courseHtml = response.css("body")
         if courseHtml.xpath("//*[@id='conteudoinner']/div[1]/a").get() is not None:
             parsed_url = urlparse(response.url)
             queryparams = parse_qs(parsed_url.query)
             course_id = queryparams['pv_curso_id'][0]
-            self.logger.warn("Found a possible pointer to another page for course with id={} at {}".format(course_id, response.meta["faculty_acronym"]))
+            self.logger.warn("Found a possible pointer to another page for course with id={} at {} (url is {})".format(course_id, response.meta["faculty_acronym"], response.url))
         # end this check
 
         # Get the data forwarded using response.meta
