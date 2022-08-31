342018c5bd0a02b79dc6bad406a6352af328f6e2
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-08-31 13:02:45 +0100

    update: pipelines, creating .sql files while scrapping

--- scrapper/scrapper/pipelines.py
+++ scrapper/scrapper/pipelines.py
@@ -8,26 +8,45 @@ import json
 import pymysql
 from . import items
 from .con_info import ConInfo
+from configparser import ConfigParser, ExtendedInterpolation
+
+config_file = "./config.ini"
+config = ConfigParser(interpolation=ExtendedInterpolation())
+config.read(config_file)
 
 
 class MySQLPipeline(ConInfo):
     def process_item(self, item, spider):
         return item
 
+    """
+        Saves the scrapped information in a specific file, so it can be easily exported. 
+    """
+    def save2file(self, item, prepared):
+        try:
+            values = tuple(map(lambda x: "\'{}\'".format(x), item.values()))
+            self.sql_file.write(prepared % values + ";\n")           
+        except Exception as e:
+            print(e)
+
 
 class FacultyPipeline(MySQLPipeline):
     def __init__(self):
         MySQLPipeline.__init__(self)
+        self.sql_file = open(config['files']['faculty'], 'a')
 
     def process_item(self, item, spider):
         if not isinstance(item, items.Faculty):
             return item
         sql = "INSERT INTO `{0}` (`{1}`) VALUES ({2})"
         prepared = sql.format('faculty', "`, `".join(item.keys()), ", ".join("%s" for _ in item.values()))
+        self.save2file(item, prepared)
         try:
             with self.connection.cursor() as cursor:
                 cursor.execute(prepared, tuple(item.values()))
                 self.connection.commit()
+        except Exception as e:
+            print(e)
         finally:
             return item
 
@@ -35,6 +54,7 @@ class FacultyPipeline(MySQLPipeline):
 class CoursePipeline(MySQLPipeline):
     def __init__(self):
         MySQLPipeline.__init__(self)
+        self.sql_file = open(config['files']['course'], 'a')
 
     def process_item(self, item, spider):
         if not isinstance(item, items.Course):
@@ -43,10 +63,13 @@ class CoursePipeline(MySQLPipeline):
         columns = "`, `".join(item.keys())
         values = ", ".join("%s" for _ in item.values())
         prepared = sql.format('course', columns, values)
+        self.save2file(item, prepared)
         try:
             with self.connection.cursor() as cursor:
                 cursor.execute(prepared, tuple(item.values()))
                 self.connection.commit()
+        except Exception as e:
+            print(e)
         finally:
             return item
 
@@ -54,6 +77,7 @@ class CoursePipeline(MySQLPipeline):
 class CourseUnitPipeline(MySQLPipeline):
     def __init__(self):
         MySQLPipeline.__init__(self)
+        self.sql_file = open(config['files']['course_unit'], 'a')
 
     def process_item(self, item, spider):
         if not isinstance(item, items.CourseUnit):
@@ -62,10 +86,13 @@ class CourseUnitPipeline(MySQLPipeline):
         columns = "`, `".join(key for key in item.keys())
         values = ", ".join("%s" for _ in item.values())
         prepared = sql.format('course_unit', columns, values)
+        self.save2file(item, prepared)
         try:
             with self.connection.cursor() as cursor:
                 cursor.execute(prepared, tuple(item.values()))
                 self.connection.commit()
+        except Exception as e:
+            print(e)
         finally:
             return item
 
@@ -73,6 +100,7 @@ class CourseUnitPipeline(MySQLPipeline):
 class SchedulePipeline(MySQLPipeline):
     def __init__(self):
         MySQLPipeline.__init__(self)
+        self.sql_file = open(config['files']['schedule'], 'a')
 
     def process_item(self, item, spider):
         if not isinstance(item, items.Schedule):
@@ -81,9 +109,12 @@ class SchedulePipeline(MySQLPipeline):
         columns = "`, `".join(item.keys())
         values = ", ".join("%s" for _ in item.values())
         prepared = sql.format('schedule', columns, values)
+        self.save2file(item, prepared)
         try:
             with self.connection.cursor() as cursor:
                 cursor.execute(prepared, tuple(item.values()))
                 self.connection.commit()
+        except Exception as e:
+            print(e)
         finally:
             return item
