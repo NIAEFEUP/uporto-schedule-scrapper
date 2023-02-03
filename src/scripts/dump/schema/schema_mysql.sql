d19f4d73760b712d1ecd4226d1a7a153915d2724
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2023-02-03 11:21:01 +0000

    refactor: fix pt links and change db encode

--- src/scripts/dump/schema/schema_mysql.sql
+++ src/scripts/dump/schema/schema_mysql.sql
@@ -11,31 +11,32 @@
 -- Table structure for table `faculty`
 --
 
+DROP TABLE IF EXISTS `faculty`;
 CREATE TABLE `faculty` (
-  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-  `acronym` varchar(10) DEFAULT NULL,
+  `acronym` varchar(255)(10) PRIMARY KEY ,
   `name` text,
   `last_updated` datetime NOT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 --
 -- Table structure for table `course`
 --
 
+DROP TABLE IF EXISTS `course`;
 CREATE TABLE `course` (
   `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `faculty_acronym` varchar(255)(10) NOT NULL, 
   `course_id` int(11) NOT NULL,
-  `faculty_id` int(11) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `acronym` varchar(10) NOT NULL,
-  `course_type` varchar(2) NOT NULL,
+  `name` varchar(255)(200) NOT NULL,
+  `acronym` varchar(255)(10) NOT NULL,
+  `course_type` varchar(255)(2) NOT NULL,
   `year` int(11) NOT NULL,
-  `url` varchar(2000) NOT NULL,
-  `plan_url` varchar(2000) NOT NULL,
+  `url` varchar(255)(2000) NOT NULL,
+  `plan_url` varchar(255)(2000) NOT NULL,
   `last_updated` datetime NOT NULL,
-  FOREIGN KEY (`faculty_id`) REFERENCES `faculty`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
-);
+  FOREIGN KEY (`faculty_acronym`) REFERENCES `faculty`(`acronym`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;;
 
 -- --------------------------------------------------------
 
@@ -43,20 +44,38 @@ CREATE TABLE `course` (
 -- Table structure for table `course_unit`
 --
 
+DROP TABLE IF EXISTS `course_unit`;
 CREATE TABLE `course_unit` (
   `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `course_unit_id` int(11) NOT NULL,
   `course_id` int(11) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `acronym` varchar(16) NOT NULL,
-  `url` varchar(2000) NOT NULL,
+  `name` varchar(255)(200) NOT NULL,
+  `acronym` varchar(255)(16) NOT NULL,
+  `url` varchar(255)(2000) NOT NULL,
   `course_year` tinyint(4) NOT NULL,
   `semester` tinyint(4) NOT NULL,
   `year` smallint(6) NOT NULL,
-  `schedule_url` varchar(2000) DEFAULT NULL,
+  `schedule_url` varchar(255)(2000) DEFAULT NULL,
   `last_updated` datetime NOT NULL,
   FOREIGN KEY (`course_id`) REFERENCES `course`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_unit_year`
+--
+
+DROP TABLE IF EXISTS `course_unit_year`;
+CREATE TABLE `course_unit_year` (
+  `course_id` INTEGER NOT NULL,
+  `course_unit_id` int(11) NOT NULL,
+  `course_unit_year` tinyint(4) NOT NULL,
+  PRIMARY KEY (`course_id`, `course_unit_id`, `course_unit_year`),
+  FOREIGN KEY (`course_unit_id`) REFERENCES `course_unit`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (`course_id`) REFERENCES `course`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 
@@ -66,20 +85,21 @@ CREATE TABLE `course_unit` (
 -- Table structure for table `schedule`
 --
 
+DROP TABLE IF EXISTS `schedule`;
 CREATE TABLE `schedule` (
   `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `day` tinyint(3) NOT NULL,
   `duration` decimal(3,1) NOT NULL,
   `start_time` decimal(3,1) NOT NULL,
-  `location` varchar(31) NOT NULL,
-  `lesson_type` varchar(3) NOT NULL,
-  `teacher_acronym` varchar(16) NOT NULL,
+  `location` varchar(255)(31) NOT NULL,
+  `lesson_type` varchar(255)(3) NOT NULL,
+  `teacher_acronym` varchar(255)(16) NOT NULL,
   `course_unit_id` int(11) NOT NULL,
   `last_updated` datetime NOT NULL,
-  `class_name` varchar(31) NOT NULL,
-  `composed_class_name` varchar(16) DEFAULT NULL,
+  `class_name` varchar(255)(31) NOT NULL,
+  `composed_class_name` varchar(255)(16) DEFAULT NULL,
   FOREIGN KEY (`course_unit_id`) REFERENCES `course_unit` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;;
 
 -- -------------------------------------------------------- 
 
@@ -90,8 +110,8 @@ CREATE TABLE `schedule` (
 --
 -- Indexes for table `course`
 --
-CREATE UNIQUE INDEX `course_course_id` ON `course` (`course_id`,`faculty_id`,`year`);
-CREATE INDEX `course_faculty_id` ON `course` (`faculty_id`); 
+CREATE UNIQUE INDEX `course_course_id` ON `course` (`course_id`,`faculty_acronym`,`year`);
+CREATE INDEX `course_faculty_acronym` ON `course` (`faculty_acronym`); 
 
 --
 -- Indexes for table `course_unit`
