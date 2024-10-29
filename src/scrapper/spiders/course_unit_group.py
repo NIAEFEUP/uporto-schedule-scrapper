40b8de174c1d893023037120a96a00ec636bdbba
Jose Sousa <2409jmsousa@gmail.com>
2024-10-29 17:57:09 +0000

    feat: inserting groups to db and improved parse

--- src/scrapper/spiders/course_unit_group.py
+++ src/scrapper/spiders/course_unit_group.py
@@ -9,7 +9,6 @@ class CourseUnitSpider(scrapy.Spider):
         super(CourseUnitSpider, self).__init__(*args, **kwargs)
         self.db = Database()
 
-        # Fetch courses to process
         sql = """
             SELECT course.id, year, faculty.acronym
             FROM course JOIN faculty 
@@ -19,12 +18,14 @@ class CourseUnitSpider(scrapy.Spider):
         self.courses = self.db.cursor.fetchall()
 
     def start_requests(self):
+        # For each course lets go to its page
         for course in self.courses:
             course_id, year, faculty_acronym = course
             url = f'https://sigarra.up.pt/{faculty_acronym}/pt/cur_geral.cur_view?pv_ano_lectivo={year}&pv_origem=CUR&pv_tipo_cur_sigla=M&pv_curso_id={course_id}'
             yield scrapy.Request(url=url, callback=self.parse_course_page, meta={'course_id': course_id, 'year': year, 'faculty_acronym': faculty_acronym})
 
     def parse_course_page(self, response):
+        #now that we are there lets get the course plan url 
         plan_link = response.xpath('//h3[text()="Planos de Estudos"]/following-sibling::div//ul/li/a/@href').extract_first()
         if plan_link:
             plan_id = plan_link.split("pv_plano_id=")[1].split("&")[0]
@@ -32,32 +33,48 @@ class CourseUnitSpider(scrapy.Spider):
             yield scrapy.Request(url=plan_url, callback=self.parse_course_plan, meta=response.meta)
 
     def parse_course_plan(self, response):
+        #ok we are in the course plan page lets get the information we need from the group tables
         group_divs = response.xpath('//div[contains(@id, "div_id_")]')
         for group_div in group_divs:
             group_title = group_div.xpath('.//h3/text()').extract_first()
             if group_title:
                 group_title = group_title.strip()
 
-                # Determine the course unit group based on the title
                 group = None
-                if "Competência Transversal" in group_title:
+                if "Competências Transversais" in group_title:
                     group = 'Competência Transversal'
-                elif "Optativa" in group_title:
+                    print("Competência Transversal")
+                elif "Grupo" in group_title:
                     group = 'Optativa'
                 if group:
                     course_rows = group_div.xpath('.//table[contains(@class, "dadossz")]/tr')
                     for row in course_rows:
-                        code = row.xpath('.//td[@class="k"]/text()').extract_first()
+                        name = row.xpath('.//td[@class="t"]/a/text()').extract_first()
+                        link = row.xpath('.//td[@class="t"]/a/@href').extract_first() #this link is crucial because it has the course unit id
+                        if link:
+                            try:
+                                course_unit_id = link.split("pv_ocorrencia_id=")[1].split("&")[0]
 
-                        if code:
-                            self.update_course_unit(code, group, response.meta['course_id'], response.meta['year'])
+                                self.update_course_unit(course_unit_id, group)
 
-    def update_course_unit(self, code, group, course_id, year):
-        """Update an existing course unit with the group and other details."""
-        sql = """
-            UPDATE course_unit 
-            SET course_group = ? 
-            WHERE id = ? AND year = ? AND course_id = ?
-        """
-        self.db.cursor.execute(sql, (group, code, year, course_id))
-        self.db.connection.commit()
+                            except IndexError:
+                                print(f"Failed to parse course unit ID from link {link}")
+
+    def update_course_unit(self, course_unit_id, group):
+        try:
+            sql = """
+                UPDATE course_unit 
+                SET course_group = ? 
+                WHERE id = ? 
+            """
+            self.db.cursor.execute(sql, (group, course_unit_id))
+            
+            self.db.connection.commit()
+
+            if self.db.cursor.rowcount > 0:
+                print(f"Successfully updated course unit {course_unit_id} with group {group}")
+            else:
+                print(f"No rows updated. Course unit {course_unit_id} may not exist.")
+
+        except Exception as e:
+            print(f"Failed to update course unit {course_unit_id} with group {group}. Error: {e}")
