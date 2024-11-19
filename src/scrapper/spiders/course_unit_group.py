5b4bfcd092ff8494617f1038d4ed41bf3891d1b8
Jose Sousa <2409jmsousa@gmail.com>
2024-11-19 18:51:52 +0000

    fix: fixxed id logic

--- src/scrapper/spiders/course_unit_group.py
+++ src/scrapper/spiders/course_unit_group.py
@@ -5,33 +5,36 @@ from ..database.Database import Database
 
 
 class CourseUnitSpider(scrapy.Spider):
-    name = "course_unit_group"
-    allowed_domains = ['sigarra.up.pt']
+    name = "course_unit_group" #This is the name of the table in the database it will be used for some queries
+    allowed_domains = ['sigarra.up.pt'] #Domain we are going to scrape
 
     def __init__(self, *args, **kwargs):
         print("Initializing CourseUnitSpider...")
         super(CourseUnitSpider, self).__init__(*args, **kwargs)
-        self.db = Database()
+        self.db = Database() 
+
+        # All this is just to get the courses from the database
 
         sql = """
             SELECT course.id, year, faculty.acronym
-            FROM course JOIN faculty 
+            FROM course JOIN faculty                                
             ON course.faculty_id = faculty.acronym
         """
+
         print("Executing SQL query to fetch courses...")
         self.db.cursor.execute(sql)
-        self.courses = self.db.cursor.fetchall()
+        self.courses = self.db.cursor.fetchall()           
         print(f"Fetched {len(self.courses)} courses from the database.")
 
-    def start_requests(self):
+    def start_requests(self): #For every course make request to the course page
         print("Starting requests...")
-        for course in self.courses:
+        for course in self.courses:         
             course_id, year, faculty_acronym = course
             print(f"Processing course: {course_id}, Year: {year}, Faculty: {faculty_acronym}")
             url = f'https://sigarra.up.pt/{faculty_acronym}/pt/cur_geral.cur_view?pv_ano_lectivo={year}&pv_origem=CUR&pv_tipo_cur_sigla=M&pv_curso_id={course_id}'
             yield scrapy.Request(url=url, callback=self.parse_course_page, meta={'course_id': course_id, 'year': year, 'faculty_acronym': faculty_acronym})
 
-    def parse_course_page(self, response):
+    def parse_course_page(self, response): #Scrape the course page , get the plan link and make request to the plan page
         print(f"Parsing course page for course ID: {response.meta['course_id']}")
         plan_link = response.xpath('//h3[text()="Planos de Estudos"]/following-sibling::div//ul/li/a/@href').extract_first()
         if plan_link:
@@ -42,7 +45,41 @@ class CourseUnitSpider(scrapy.Spider):
         else:
             print(f"No Planos de Estudos link found for course ID: {response.meta['course_id']}")
 
-    def parse_course_plan(self, response):
+    #Auxiliary functions for parse_course_plan
+
+    def course_unit_exists_already(self, course_unit_id):
+        sql = """
+        SELECT count(*) 
+        FROM course_unit WHERE id = ?
+        """
+        self.db.cursor.execute(sql, (course_unit_id,))
+        result = self.db.cursor.fetchone()
+        return result[0] > 0
+
+    def course_group_index(self, course_group_name, course_id):
+        sql = """
+        SELECT course_group.id
+        FROM course_group 
+        WHERE name = ? 
+        AND course_id = ?
+        """
+        self.db.cursor.execute(sql, (course_group_name, course_id))
+        result = self.db.cursor.fetchone()
+        if result:
+            return result[0]
+        return None
+
+    def get_next_group_id(self):
+        sql = """
+        SELECT MAX(id) FROM course_group
+        """
+        self.db.cursor.execute(sql)
+        result = self.db.cursor.fetchone()
+        if  result[0]:
+            return result[0] + 1
+        return 1
+
+    def parse_course_plan(self, response): #Scrape the course plan page to get course groups and the course units in each group then just yield the items
         print(f"Parsing course plan for course ID: {response.meta['course_id']}")
         group_divs = response.xpath('//div[contains(@id, "div_id_")]')
         course_id = response.meta['course_id']
@@ -52,10 +89,6 @@ class CourseUnitSpider(scrapy.Spider):
             if group_title:
                 group_name = group_title.strip()
                 print(f"Found group: {group_name} for course ID: {course_id}")
-                hash_object = hashlib.md5(group_name.encode('utf-8'))
-                hashed_group_name = hash_object.hexdigest()
-                numeric_group_id = int(hashed_group_name, 16) % 10**8  
-                print(f"Generated numeric group ID: {numeric_group_id}")
                 course_rows = group_div.xpath('.//table[contains(@class, "dadossz")]/tr')
                 for row in course_rows:
                     name = row.xpath('.//td[@class="t"]/a/text()').extract_first()
@@ -65,25 +98,22 @@ class CourseUnitSpider(scrapy.Spider):
                             course_unit_id = link.split("pv_ocorrencia_id=")[1].split("&")[0]
                             print(f"Found course unit ID: {course_unit_id} in group: {group_name}")
 
-                            sql = """
-                            SELECT count(*) 
-                            FROM course_unit WHERE id = ?
-                            """
-                            self.db.cursor.execute(sql, (course_unit_id,))
-                            result = self.db.cursor.fetchone()
-                            if result[0] > 0:
+                            if self.course_unit_exists_already(course_unit_id):
                                 # Yield CourseGroup item
-                                course_group_item = CourseGroup(
-                                    id=numeric_group_id,
-                                    name=group_name,
-                                    course_id=course_id
-                                )
-                                yield course_group_item 
+                                course_group_id = self.course_group_index(group_name, course_id)
+                                if not course_group_id:
+                                    course_group_id = self.get_next_group_id()
+                                    course_group_item = CourseGroup(
+                                        id=course_group_id,
+                                        name=group_name,
+                                        course_id=course_id
+                                    )
+                                    yield course_group_item 
 
                                 # Yield CUCG item 
                                 course_unit_course_group_item = CUCG(
                                     course_unit_id=course_unit_id,
-                                    course_group_id=numeric_group_id,
+                                    course_group_id=course_group_id,
                                 )
                                 yield course_unit_course_group_item
                             else:
