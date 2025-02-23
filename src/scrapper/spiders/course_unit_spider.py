e11bdcd166f00f2b24ae5651e71911312ac5f718
Jose Sousa <2409jmsousa@gmail.com>
2025-02-23 09:41:39 +0000

    fix:removed print

--- src/scrapper/spiders/course_unit_spider.py
+++ src/scrapper/spiders/course_unit_spider.py
@@ -140,7 +140,6 @@ class CourseUnitSpider(scrapy.Spider):
                 
                 study_cycles = response.xpath('//h3[text()="Ciclos de Estudo/Cursos"]/following-sibling::table[1]').get()
                 df = pd.read_html(study_cycles, decimal=',', thousands='.', extract_links="all")[0]
-                print(df)
                 for (_, row) in df.iterrows():
                         yield CourseCourseUnit(
                                 course_id= parse_qs(urlparse(row[0][1]).query).get('pv_curso_id')[0],
